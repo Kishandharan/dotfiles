11ad875137e2caa25ff32f95337622efc9b4043f
Kishandharan <kishanrajeevi.sangamone@gmail.com>
2026-01-14 05:55:13 +0400

    Made background color darker

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,7 @@ vim.g.nightflyWinSeparator = 3
 vim.g.nightflyItalics = false
 vim.g.nightflyNormalFloat = true
 require("nightfly").custom_colors({ -- This block should be removed or commented if transprancy = true
-  bg = "#08091a",
+  bg = "#060714",
 })
 require("oil").setup()
 vim.cmd("set wrap linebreak nolist")
