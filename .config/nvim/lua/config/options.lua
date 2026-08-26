d00a12faf340e284302cedaca2a69caa39d05d48
Kishandharan <kishanrajeevi.sangamone@gmail.com>
2026-08-26 15:46:55 +0530

    Updated snipe.nvim settings
    Snipe.nvim config changes:
    
    1. Moved the window to bottom right
    2. Changed the window title to "Buffers"
    3. Changed keymap for closing window to "q"

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,4 +2,14 @@ vim.opt.termguicolors = true
 vim.opt.relativenumber = false
 vim.opt.jumpoptions = "stack"
 vim.cmd("set wrap linebreak nolist")
-require("snipe").setup()
+require("snipe").setup({
+  ui = {
+    position = "bottomright",
+    open_win_override = {
+      title = "Buffers",
+    }
+  },
+  navigate = {
+    cancel_snipe = "q"
+  }
+})
